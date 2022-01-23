ac4c2d1384a48313d7f467efdadfbd12cb4feb58
Jumpei Funayama <contact.zouri.music@gmail.com>
2022-01-24 01:27:14 +0900

    ユーザー追加
    adminとuserを用意

--- zenfin/config/routes.rb
+++ zenfin/config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
+  root to: 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
