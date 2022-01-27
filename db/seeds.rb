84ef9ec266e6f5a79110a5c231d905b8d3874591
Jumpei Funayama <contact.zouri.music@gmail.com>
2022-01-27 18:18:42 +0900

    デモデータの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,3 +27,39 @@ Sspn.find_or_create_by(id: 99) do |sspn|
     sspn.season = ""
     sspn.remarks = "特支なし用"
 end
+
+#以降はデモ用のデータ
+
+User.find_or_create_by(id: 2) do |admin|
+    admin.email = "user2@example.com"
+    admin.password = "password"
+end
+
+User.find_or_create_by(id: 3) do |admin|
+    admin.email = "user3@example.com"
+    admin.password = "password"
+end
+
+Sspn.find_or_create_by(id: 1) do |sspn|
+    sspn.amount = 300000
+    sspn.number = "20220130-SKN-001"
+    sspn.grant = 1
+    sspn.ai = "SKN"
+    sspn.advance = 1
+    sspn.item = "交通費"
+    sspn.reason = "2022年1月"
+    sspn.season = "委員の移動費のため"
+    sspn.remarks = "テスト用データ"
+end
+
+Sspn.find_or_create_by(id: 2) do |sspn|
+    sspn.amount = 10000
+    sspn.number = "20220130-SKN-002"
+    sspn.grant = 1
+    sspn.ai = "SKN"
+    sspn.advance = 1
+    sspn.item = "贈答品日"
+    sspn.reason = "2022年1月"
+    sspn.season = "印刷所へのお礼のため"
+    sspn.remarks = "テスト用データ"
+end
